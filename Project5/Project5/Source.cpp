9a9eb667ea03eec53f22a88dc6b201be5c1ba6e5
Blizzard-Nova <55759750+Blizzard-Nova@users.noreply.github.com>
2021-08-13 21:04:53 +0500

    Add files via upload

---
+++ Project5/Project5/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+	/*int* intptr = new int;
+	char* namestr = new char[8];
+	*intptr = 357;
+	strcpy(namestr, "datastructure");*/
+
+	/*int* ptr1 = new int;
+	int* ptr2 = new int;
+	*ptr2 = 44;
+	*ptr1 = *ptr2;
+	int x = 5;
+	ptr1 = ptr2;
+	delete ptr2;*/
+	
+	char baka[5] = { 'a','b','v','o','\0' };
+	cout << baka;
+	
+	return 0;
+
+}
